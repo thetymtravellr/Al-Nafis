fdfa86a1a7aed14b2d37a0eaa09045fb47935e27
Robiul Hasan <muhammadrobiul502@gmail.com>
2022-04-17 19:42:35 +0600

    notfound page added

--- src/Pages/NotFound/NotFound.js
+++ src/Pages/NotFound/NotFound.js
@@ -1,14 +1,25 @@
-import React from 'react';
-import ghost from '../../Assets/images/icons/ghost.png';
+import { ArrowNarrowLeftIcon } from "@heroicons/react/outline";
+import React from "react";
+import { Link } from "react-router-dom";
+import ghost from "../../Assets/images/icons/ghost.png";
 
 const NotFound = () => {
-    return (
-        <section className='min-h-screen  max-w-7xl mx-auto flex justify-center items-center my-20 lg:my-0 relative'>
-            <div>
-                <img src={ghost} alt="" />
-            </div>
-        </section>
-    );
+  return (
+    <section className="h-[75vh] w-full grid place-content-center my-20 lg:my-0 relative">
+      <div className="text-center font-poppins text-gray-500">
+        <img className="w-1/4 mx-auto mb-12" src={ghost} alt="" />
+        <h1 className="text-4xl font-bold mb-6">Whoops!</h1>
+        <p className="text-2xl mb-6">
+          We couldn't find the page <br /> you looking for.
+        </p>
+        <Link to="/" className="flex justify-center">
+          <button className="flex gap-3 bg-gray-200 py-3 px-6 rounded-xl hover:bg-gray-300 ">
+            <ArrowNarrowLeftIcon className="h-6" /> Go Home
+          </button>
+        </Link>
+      </div>
+    </section>
+  );
 };
 
 export default NotFound;
