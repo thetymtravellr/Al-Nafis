623976c658ec8f83f8563c953a2641fc9d16e453
Robiul Hasan <muhammadrobiul502@gmail.com>
2022-04-17 14:10:56 +0600

    banner added

--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -1,8 +1,10 @@
 import React from 'react';
+import Banner from '../../Components/Banner/Banner';
 
 const Home = () => {
     return (
         <main>
+            <Banner></Banner>
             
         </main>
     );
